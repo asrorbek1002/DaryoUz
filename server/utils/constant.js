f6c230214da36b062184c922da5d9b8cc7b3aa08
Asrorbek <151444045+asrorbek1002@users.noreply.github.com>
2025-01-18 15:51:11 +0500

    Update constant.js

--- server/utils/constant.js
+++ server/utils/constant.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongo: "mongodb://127.0.0.1:27017/daryo",
+    mongo: "mongodb+srv://asrorbekaliqulov08:DJF8d2gk1TMJm45u@cluster0.tfjy0.mongodb.net/",
     server: 6060,
     sessionKeyword: "63da69c13ecf6b533f3c4ede10222e27",
     sessionTime: 1000 * 60 * 60 * 2,
